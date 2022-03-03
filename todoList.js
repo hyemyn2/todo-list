f8414a02f4bcb6a25df29d21f8cc45dfcf9b045d
hyemyn2 <hyemyn2@gmail.com>
2022-03-03 11:28:47 +0900

    UI 및 이벤트 작성
    - todo list UI 작성
    - 이벤트 함수 작성

---
+++ todoList.js
@@ -0,0 +1,133 @@
+function todoList (){
+    var inputList = document.querySelector('input[id=inputList]')
+    var addBtn = document.querySelector('.addBtn')
+    var clearBtn = document.querySelector('.clearBtn')
+    var ulTodo = document.querySelector('.todos')
+    var listArr = []
+    var dataNum = 0
+
+    // -------------------- event handler --------------------
+
+    addBtn.addEventListener('click', function (e) { addTodo(e) })
+    clearBtn.addEventListener('click', function (e) { clearLocal(e) })
+    ulTodo.addEventListener('click', function (e) { deleteTodo(e) })
+    ulTodo.addEventListener('click', function (e) { checkTodo(e) })
+
+    // -------------------- event function --------------------
+
+    // add list
+    function addTodo (e){
+        e.preventDefault();
+        // const newTodo = inputList.value
+        const newTodo = {text: inputList.value, checked: false}
+
+        if(inputList.value !== '') {
+            listArr.push(newTodo);
+            saveLocal()
+            inputList.focus()
+        }
+            inputList.value = '';
+    }
+
+    // 
+
+    // delete list
+    function deleteTodo(e){
+        if (e.target.className.includes('delBtn') === true) {
+            dataNum = e.target.closest('li').dataset.num
+            listArr.splice(dataNum, 1)
+            // console.log(listArr)
+
+            saveLocal()
+        }
+    }
+
+    // check list
+    function checkTodo(e){
+        if(!e.target.classList.contains('checkBtn')){
+            return
+        }
+    // var ulTodo = document.querySelector('.todos')
+
+        dataNum = e.target.closest('li').dataset.num
+        const localData = JSON.parse(localStorage.getItem('testing'))
+        localData[dataNum].checked = !localData[dataNum].checked
+        listArr = localData
+        // console.log(listArr)
+        saveLocal()
+    }
+
+    // save localstorage
+    function saveLocal () {
+        localStorage.setItem('testing', JSON.stringify(listArr))
+        showTodo()
+    }
+
+    // list rendering
+    function showTodo (){
+
+        if (listArr.length === 0) {
+            const middleSection = document.querySelector('.middleSection')
+            middleSection.classList.add('noArray')
+        } else {
+            const middleSection = document.querySelector('.middleSection')
+            middleSection.classList.remove('noArray')
+        }
+
+        const renderLi = listArr.map( (obj, i) => {
+
+            // ----------------------------------------------------------------- //
+
+            return `<li class="todo ${obj.checked===true ? 'checked' : ''}" data-num="${i}">
+                        <span class="todoText">${obj.text}</span>
+                        <button class="btn checkBtn"><i class="fa-solid fa-check"></i></button>
+                        <button class="btn delBtn"><i class="fa-solid fa-xmark"></i></button>
+                    </li>`
+        }).join('')
+        ulTodo.innerHTML = renderLi;
+    }
+
+
+
+    function clearLocal (){
+        localStorage.clear()
+        listArr = []
+        showTodo()
+    }
+    
+    // init
+    function initTodo (){
+        if (!localStorage.testing){
+            listArr = []
+        } else {
+            const getList = localStorage.getItem('testing')
+            const initList = JSON.parse(getList)
+                listArr.push(...initList)
+            showTodo();
+        }
+    }
+    initTodo()
+}
+
+window.addEventListener('DOMContentLoaded', todoList)
+
+
+
+
+
+
+
+
+
+
+        // let newList;
+        // if(!localStorage.test){
+        //     newList = { num: 1, txt: newTodo}
+        // } else {
+        //     newList = { num: localStorage.test.length+1, txt: newTodo}
+        // }
+
+        // listArr.push(newList)
+
+        // localStorage.setItem('test', JSON.stringify(listArr))
+        // // JSON.parse( localstorage key )
